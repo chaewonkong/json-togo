3948ee3e38ee91fe04bd60fa65f433d675c65e46
chaewonkong <chaewonkong@gmail.com>
2025-06-05 10:36:01 +0900

    feat: recursive struct

--- structstr/generate.go
+++ structstr/generate.go
@@ -12,13 +12,27 @@ func Generate(jsonMap map[string]any, pkgName, structName string) string {
 	sb.WriteString(fmt.Sprintf("package %s\n\n", pkgName))
 	sb.WriteString(fmt.Sprintf("type %s struct {\n", structName))
 
-	// iterate map and generate struct fields
+	return generate(&sb, jsonMap, 4)
+}
+
+// TODO: test code
+func generate(sb *strings.Builder, jsonMap map[string]any, tab int) string {
+	if tab < 4 {
+		tab = 4 // minimum indentation
+	}
+
+	indent := strings.Repeat(" ", tab)
 	for key, val := range jsonMap {
 		fieldName := ToPascalCase(key)
 		fieldType := InferTypeString(val)
-		sb.WriteString(fmt.Sprintf("    %s %s `json:\"%s\"`\n", fieldName, fieldType, key))
+		if fieldType == "map[string]any" {
+			sb.WriteString(fmt.Sprintf("%s%s struct {\n", indent, fieldName))
+			generate(sb, val.(map[string]any), tab+4)
+			continue
+		}
+		sb.WriteString(fmt.Sprintf("%s%s %s `json:\"%s\"`\n", indent, fieldName, fieldType, key))
 	}
-	sb.WriteString("}")
+	sb.WriteString(fmt.Sprintf("%s}\n", strings.Repeat(" ", tab-4)))
 
 	return sb.String()
 }
